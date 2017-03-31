ada80eb5a163e275f7ada05469cab0be40cc48be
Miłosz Ryguła <miloszrygula@interia.pl>
2017-04-01 01:09:45 +0200

    Add tileMove function

--- sokoban.c
+++ sokoban.c
@@ -4,6 +4,14 @@
 #include <ncurses.h>
 #include <inttypes.h>
 
+#define TILE_AIR_ID 0
+#define TILE_PLAYER_ID 1
+#define TILE_SOCKET_ID 2
+#define TILE_BOX_ID 3
+#define TILE_SOCKETPLAYER_ID 4
+#define TILE_SOCKETBOX_ID 5
+#define TILE_WALL_ID 6
+
 typedef struct
 {
 	uint16_t id;
@@ -11,14 +19,14 @@ typedef struct
 	char chr;
 
 } Tile;
-//								ID	FG	BG	CHAR
-const Tile TILE_AIR = 			{0,	0,	0,	' ' };
-const Tile TILE_PLAYER = 		{1,	7,	0,	'@' };
-const Tile TILE_SOCKET = 		{2,	0,	4,	' '	};
-const Tile TILE_BOX = 			{3,	2,	0,	'#' };
-const Tile TILE_SOCKETPLAYER = 	{4,	7,	4,	'@' };
-const Tile TILE_SOCKETBOX = 	{5,	3,	5,	'#'	};
-const Tile TILE_WALL = 			{6,	0,	6,	' ' };
+//								ID						FG	BG	CHAR
+const Tile TILE_AIR = 			{TILE_AIR_ID,			0,	0,	' ' };
+const Tile TILE_PLAYER = 		{TILE_PLAYER_ID,		7,	0,	'@' };
+const Tile TILE_SOCKET = 		{TILE_SOCKET_ID,		0,	4,	' '	};
+const Tile TILE_BOX = 			{TILE_BOX_ID,			2,	0,	'#' };
+const Tile TILE_SOCKETPLAYER = 	{TILE_SOCKETPLAYER_ID,	7,	4,	'@' };
+const Tile TILE_SOCKETBOX = 	{TILE_SOCKETBOX_ID,		3,	5,	'#'	};
+const Tile TILE_WALL = 			{TILE_WALL_ID,			0,	6,	' ' };
 
 struct map
 {
@@ -63,6 +71,106 @@ void colorsInit( )
 			init_pair( fg * 8 + bg, fg, bg );
 }
 
+int tileMove( uint16_t x, uint16_t y, int8_t dx, int8_t dy )
+{
+	Tile *obj, *dest;
+
+	//Deltas limit
+	if ( dx ) dx = dx < 0 ? -1 : 1;
+	if ( dy ) dy = dy < 0 ? -1 : 1 ;
+
+	//Check terminal borders
+	if( x < 0 || y < 0 || x >= map.width || y >= map.height ) return 0;
+	if( x + dx < 0 || y + dy < 0 || x + dx >= map.width || y + dy >= map.height ) return 0;
+
+	//Get pointers for convenience
+	obj = &map.map[x][y];
+	dest = &map.map[x + dx][y + dy];
+
+	//Returned valuse
+	//0 - unable to move
+	//1 - succesfully moved
+
+	switch( obj->id )
+	{
+		//Player or player on socket
+		case TILE_SOCKETPLAYER_ID:
+		case TILE_PLAYER_ID:
+			switch ( dest->id )
+			{
+				//Always can be moved to empty field
+				case TILE_AIR_ID:
+					*obj = ( obj->id == TILE_PLAYER_ID ? TILE_AIR : TILE_SOCKET );
+					*dest = TILE_PLAYER;
+					return 1;
+					break;
+				//Player can push box if box can be moved
+				case TILE_BOX_ID:
+					if ( tileMove( x + dx, y + dy, dx, dy ) )
+					{
+						*obj = ( obj->id == TILE_PLAYER_ID ? TILE_AIR : TILE_SOCKET );
+						*dest = TILE_PLAYER;
+						return 1;
+					}
+					return 0;
+					break;
+
+				//Player always can stand on another socket
+				case TILE_SOCKET_ID:
+					*obj = ( obj->id == TILE_PLAYER_ID ? TILE_AIR : TILE_SOCKET );
+					*dest = TILE_SOCKETPLAYER;
+					return 1;
+					break;
+
+				//Player can push box from full socket if box can be moved
+				case TILE_SOCKETBOX_ID:
+					if ( tileMove( x + dx, y + dy, dx, dy ) )
+					{
+						*obj = ( obj->id == TILE_PLAYER_ID ? TILE_AIR : TILE_SOCKET );
+						*dest = TILE_SOCKETPLAYER;
+						return 1;
+					}
+					return 0;
+					break;
+
+				//in case of other cases (e.g. walls)
+				default:
+				 	return 0;
+					break;
+
+			}
+		break;
+
+		//Box or box on socket
+		case TILE_BOX_ID:
+		case TILE_SOCKETBOX_ID:
+			switch ( dest->id )
+			{
+				//Box always can be moved onto empty field
+				case TILE_AIR_ID:
+					*obj = ( obj->id == TILE_BOX_ID ? TILE_AIR : TILE_SOCKETBOX);
+					*dest = TILE_BOX;
+					return 1;
+					break;
+
+				//Box can be always moved onto socket
+				case TILE_SOCKET_ID:
+					*obj = ( obj->id == TILE_BOX_ID ?TILE_AIR : TILE_SOCKETBOX);
+					*dest = TILE_SOCKETBOX;
+					return 1;
+					break;
+
+				default:
+					return 0;
+					break;
+
+			}
+		break;
+	}
+
+	return 0;
+}
+
 int main()
 {
 	if( initscr() == NULL )
@@ -77,9 +185,6 @@ int main()
 	start_color();
 	colorsInit( );
 	mapInit( );
-	map.map[0][0] = TILE_SOCKETBOX;
-	map.map[1][0] = TILE_WALL;
-	map.map[0][1] = TILE_PLAYER;
 	mapDraw( );
 	getch( );
 	endwin( );
